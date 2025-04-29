5b657421ea623ef0023fd63979b44778e87fc3e2
EmmanuelKeifala <emmanuelkeifala@gmail.com>
2025-04-29 02:13:13 +0000

    [UPDATE]:  basic routes for getting and deleting

--- crud-api/main.go
+++ crud-api/main.go
@@ -1,5 +1,14 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
 type Movie struct {
 	ID       string    `json:"id"`
 	Isbn     string    `json:"isbn"`
@@ -9,5 +18,73 @@ type Movie struct {
 
 type Director struct {
 	Firstname string `json:"firstname"`
-	Lastname string `json:"lastname"`
+	Lastname  string `json:"lastname"`
+}
+
+var movies []Movie
+
+func getMovies(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(movies)
+
+}
+
+func deleteMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	param := mux.Vars(r)
+	for index, item := range movies {
+		if item.ID == param["id"] {
+			movies = append(movies[:index], movies[index+1:]...)
+			break
+		}
+	}
+
+	json.NewEncoder(w).Encode(movies)
+}
+
+func getMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	param := mux.Vars(r)
+
+	for _, item := range movies {
+		if item.ID == param["id"] {
+			json.NewEncoder(w).Encode(item)
+		}
+	}
+}
+func main() {
+	r := mux.NewRouter()
+
+	// create dummy movies
+	movies = append(movies, Movie{
+		ID:    "1",
+		Isbn:  "323423",
+		Title: "movie 1",
+		Director: &Director{
+			Firstname: "Emmanuel",
+			Lastname:  "Keifala",
+		},
+	})
+	movies = append(movies, Movie{
+		ID:    "3",
+		Isbn:  "32das423",
+		Title: "movie 3",
+		Director: &Director{
+			Firstname: "Grim",
+			Lastname:  "Kel",
+		},
+	})
+
+	// Handle routes
+	r.HandleFunc("/movies", getMovies).Methods("GET")
+	r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
+	// r.HandleFunc("/movies", createMovie).Methods("POST")
+	// r.HandleFunc("/movies/{id}", updateMovie).Methods("PUT")
+	r.HandleFunc("/movies/{id}", deleteMovie).Methods("DELETE")
+
+	// start server
+	fmt.Printf("Starting server on port 8000")
+	log.Fatal(http.ListenAndServe(":8000", r))
+
 }
