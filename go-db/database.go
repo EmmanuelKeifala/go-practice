0f1132fe90e95056234bc0d48681cab4439ae138
EmmanuelKeifala <emmanuelkeifala@gmail.com>
2025-04-29 00:33:10 +0000

    [UPDATE]: Implement tree insertion logic and node handling in BTree

--- go-db/database.go
+++ go-db/database.go
@@ -180,4 +180,53 @@ func nodeSplit3(old BNode) (uint16, [3]BNode) {
 	assert(leftLeft.nbytes() <= BTREE_PAGE_SIZE)
 
 	return 3, [3]BNode{leftLeft, middle, right} // 3 nodes
+	// they are all just temporary data until the nodeReplaceKidN actually allocates them
+
+}
+
+// insert a KV into a node, the result might be split
+// the caller is responsivle for deallocationg the input node
+//  and splitting and allocationg result nodes
+
+func treeInsert(tree *BTree, node BNode, key []byte, val []byte) BNode {
+	//  the result node
+	//  it's allowed to be bigger than 1 page and will be split if so
+	new := BNode(make([]byte, 2*BTREE_PAGE_SIZE))
+	// where to insert the key?
+	idx := nodeLookUpLE(node, key)
+	switch node.btype() {
+	case BNODE_LEAF:
+		// leaf, node.getKey(idx) <= key
+		if bytes.Equal(key, node.getKey(idx)) {
+			// found the key, update it
+			// leafUpdate(new, node, idx, key, val)
+
+		} else {
+			// insert it fter the position
+			leafInsert(new, node, idx+1, key, val)
+		}
+	case BNODE_NODE:
+		//  internal node, insert it to a kid node
+		nodeInsert(tree, new, node, idx, key, val)
+
+	default:
+		panic("bad node!")
+	}
+
+	return new
+}
+
+// part of the treeInsert(): KV insertion to an internl node
+func nodeInsert(tree *BTree, new BNode, node BNode, idx uint16, key []byte, val []byte) {
+	kptr := node.getPtr(idx)
+	// recursive insertion to the kid node
+	knode := treeInsert(tree, tree.get(kptr), key, val)
+
+	// split the result
+	nsplit, split := nodeSplit3(knode)
+
+	// deallocate the kid node
+	tree.del(kptr)
+
+	nodeReplaceKidN(tree, new, node, idx, split[:nsplit]...)
 }
