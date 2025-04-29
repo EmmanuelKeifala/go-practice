f86feeaf0f38ade5b18e1bb92edabbdbc022518a
EmmanuelKeifala <emmanuelkeifala@gmail.com>
2025-04-29 01:34:45 +0000

    [UPDATE]: route handling with http

--- web-server/go.mod
+++ web-server/go.mod
@@ -1,3 +1,3 @@
-module webserver
+module webserver/m
 
 go 1.23.4
--- web-server/main.go
+++ web-server/main.go
@@ -1,11 +1,60 @@
 package main
 
-import {
+import (
 	"fmt"
 	"log"
 	"net/http"
+)
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 not found", http.StatusNotFound)
+		return
+	}
+
+	if r.Method != "GET" {
+		http.Error(w, "method not supported", http.StatusNotFound)
+		return
+	}
+
+	fmt.Fprintf(w, "hello")
 }
 
-func main(){
-	
+func formHandler(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "Parseform Error %v", err)
+
+		return
+	}
+	if r.URL.Path != "/form" {
+		http.Error(w, "404 not found", http.StatusNotFound)
+		return
+	}
+	if r.Method != "POST" {
+		http.Error(w, "method not supported", http.StatusNotFound)
+		return
+	}
+
+	fmt.Fprintf(w, "Form posted correctly")
+
+	name := r.FormValue("name")
+	address := r.FormValue("address")
+
+	fmt.Fprintf(w, "Name=%s\n", name)
+	fmt.Fprintf(w, "Address=%s\n", address)
+
+}
+
+func main() {
+	file_server := http.FileServer(http.Dir("./static")) // point the file server to the static files
+
+	// route handling
+	http.Handle("/", file_server)
+	http.HandleFunc("/form", formHandler)
+	http.HandleFunc("/hello", helloHandler)
+	fmt.Printf("Starting server at port 8000\n")
+
+	if err := http.ListenAndServe(":8000", nil); err != nil {
+		log.Fatal(err)
+	}
 }
