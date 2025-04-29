fccd49750fe58d687298ae84db4e2c71fc6f0809
EmmanuelKeifala <emmanuelkeifala@gmail.com>
2025-04-29 08:09:02 +0000

    [FEAT]: structure and layout

---
+++ bookstore/pkg/models/book.model.go
@@ -0,0 +1,24 @@
+package models
+
+import (
+	"github.com/EmmanuelKeifala/go-practice/bookstore/pkg/config"
+	"github.com/jinzhu/gorm"
+)
+
+var (
+	db *gorm.DB
+)
+
+type Book struct {
+	gorm.Model
+	Name        string `json:"name"`
+	Author      string `json:"author"`
+	Publication string `json:"publication"`
+}
+
+func init() {
+	config.Connect()
+	db = config.GetDB()
+
+	db.AutoMigrate(&Book{})
+}
