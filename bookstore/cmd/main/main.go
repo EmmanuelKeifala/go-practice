bd96d9c197ccceec1b6e2930fc3a34897a7ec60b
EmmanuelKeifala <emmanuelkeifala@gmail.com>
2025-04-30 21:40:53 +0000

    [UPDATE]: Implement CRUD operations for books and set up main HTTP server

--- bookstore/cmd/main/main.go
+++ bookstore/cmd/main/main.go
@@ -1,1 +1,18 @@
 package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/EmmanuelKeifala/go-practice/bookstore/pkg/routes"
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	r := mux.NewRouter()
+
+	routes.RegisterBookStroreRoutes(r)
+
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe("localhost:9010", r))
+}
--- bookstore/pkg/routes/bookstore.route.go
+++ bookstore/pkg/routes/bookstore.route.go
@@ -5,9 +5,9 @@ import (
 	"github.com/gorilla/mux"
 )
 
-var RegisterBookStroreRoutes = func(router mux.Router) {
+var RegisterBookStroreRoutes = func(router *mux.Router) {
 	router.HandleFunc("/book/", controllers.CreateBook).Methods("POST")
-	router.HandleFunc("/book/", controllers.GetBook).Methods("GET")
+	router.HandleFunc("/book/", controllers.GetAllBooks).Methods("GET")
 	router.HandleFunc("/book/{bookId}", controllers.GetBookById).Methods("GET")
 	router.HandleFunc("/book/{bookId}", controllers.UpdateBook).Methods("PUT")
 	router.HandleFunc("/book/{bookId}", controllers.DeleteBook).Methods("DELETE")
